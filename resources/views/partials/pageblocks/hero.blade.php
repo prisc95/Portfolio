77ffa10330f33bc4fcd7a4656ca2d863df0e89d2
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-03-07 22:19:47 +0100

    First setup hero section

---
+++ resources/views/partials/pageblocks/hero.blade.php
@@ -0,0 +1,14 @@
+<section class="hero">
+    <div class="left">
+        <div class="top"><h1>Jouw visie, mijn code. <span class="gradient-text">Alles begint met 1 pixel per keer.</span></h1></div>
+        <div class="middle"><p>Gedreven Junior Frontend Developer met een passie voor het omzetten van prachtige designs naar responsieve websites, altijd op zoek naar nieuwe kansen om mijn vaardigheden verder te ontwikkelen. </p></div>
+        <div class="btn-wrapper">
+            <button class="contact-btn">Laten we in contact komen</button>
+            <button class="portfolio-btn">Bekijk mijn werk (pijltje)</button>
+        </div>
+    </div>
+
+    <div class="right">
+        <img src="wp-content/themes/sage/resources/images/Profielfoto.png" alt="">
+    </div>
+</section>
